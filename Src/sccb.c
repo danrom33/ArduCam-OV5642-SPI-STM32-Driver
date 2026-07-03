7cb2e88cd6922f067d3a101384857804a5bc59e5
Dan Rom <danrom2210@gmail.com>
2026-07-03 16:34:49 +0200

    Update sccb.c
    Fix typo in enmum error

--- Src/sccb.c
+++ Src/sccb.c
@@ -30,18 +30,18 @@ OV5642_StatusTypeDef SCCB_read_reg(uint16_t reg_addr, uint8_t *val){
   addr[0] = (reg_addr >> 8) & 0xFF; //High byte
   addr[1] = (reg_addr & 0xFF); //low byte
   if(HAL_I2C_IsDeviceReady(hov5642.hi2c, OV5642_WRITE_ADDR, 100, 200) != HAL_OK){
-    return OV5642_I2CTIMEOUT;
+    return OV5642_I2C_TIMEOUT;
   }
   HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(hov5642.hi2c, OV5642_WRITE_ADDR, addr, 2, HAL_MAX_DELAY);
    if(status != HAL_OK){
     if(status == HAL_TIMEOUT)
-        return OV5642_I2CTIMEOUT;
+        return OV5642_I2C_TIMEOUT;
     return OV5642_ERROR;
   }
   status = HAL_I2C_Master_Receive(hov5642.hi2c, OV5642_READ_ADDR, val, 1, HAL_MAX_DELAY);
    if(status != HAL_OK){
     if(status == HAL_TIMEOUT)
-        return OV5642_I2CTIMEOUT;
+        return OV5642_I2C_TIMEOUT;
     return OV5642_ERROR;
   }
 //   printf("Received: Reg 0x%02x: 0x%02x\r\n", reg_addr, val);
